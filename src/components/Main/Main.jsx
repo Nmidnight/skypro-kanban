b647d30810ac1074af1c453aa03b700aac6902aa
Nmidnight <bynanamid46@gmail.com>
2025-12-26 13:12:25 +0200

    loading done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,37 @@
 import "./Main.css";
 import { Column } from "../Column/Column";
 import { cardList } from "../../../data.js";
+import { useEffect, useState } from "react";
 
 export function Main() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <main className="main">
       <div className="container">
-        <div className="main__block">
-          <div className="main__content">
-            <Column cards={cardList} title="Без статуса" />
-            <Column cards={cardList} title={"Нужно сделать"} />
-            <Column cards={cardList} title="В работе" />
-            <Column cards={cardList} title={"Тестирование"} />
-            <Column cards={cardList} title={"Готово"} />
+        {isLoading ? (
+          <div className="loader">
+            <p>Данные загружаются...</p>
           </div>
-        </div>
+        ) : (
+          <div className="main__block">
+            <div className="main__content">
+              <Column cards={cardList} title="Без статуса" />
+              <Column cards={cardList} title={"Нужно сделать"} />
+              <Column cards={cardList} title="В работе" />
+              <Column cards={cardList} title={"Тестирование"} />
+              <Column cards={cardList} title={"Готово"} />
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
