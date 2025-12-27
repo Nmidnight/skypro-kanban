60eece4b465cf976af7f6d555dfd1d83e1104e75
Nmidnight <bynanamid46@gmail.com>
2025-12-27 22:13:23 +0200

    fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,27 +23,28 @@ export function Header() {
               <a href="#popNewCard">Создать новую задачу</a>
             </button>
             <a
-              href="#user-set-target"
               className="header__user _hover02"
               onClick={() => setClicked((clicked) => !clicked)}
             >
               Ivan Ivanov
             </a>
-            <div
-              className="header__pop-user-set pop-user-set"
-              id="user-set-target"
-              style={{ display: clicked ? "block" : "none" }}
-            >
-              <p className="pop-user-set__name">Ivan Ivanov</p>
-              <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-              <div className="pop-user-set__theme">
-                <p>Темная тема</p>
-                <input type="checkbox" className="checkbox" name="checkbox" />
+
+            {clicked && (
+              <div
+                className="header__pop-user-set pop-user-set"
+                id="user-set-target"
+              >
+                <p className="pop-user-set__name">Ivan Ivanov</p>
+                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                <div className="pop-user-set__theme">
+                  <p>Темная тема</p>
+                  <input type="checkbox" className="checkbox" name="checkbox" />
+                </div>
+                <button type="button" className="_hover03">
+                  <a href="#popExit">Выйти</a>
+                </button>
               </div>
-              <button type="button" className="_hover03">
-                <a href="#popExit">Выйти</a>
-              </button>
-            </div>
+            )}
           </nav>
         </div>
       </div>
